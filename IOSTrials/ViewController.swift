6e5b2ac3e4c5dea5b4d423e453afd1163cf27c75
ArthurJiang <arthursjiang@hotmail.com>
2015-09-06 23:27:57 +0800

    Initial Commit

---
+++ IOSTrials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSTrials
+//
+//  Created by ArthurJiang on 9/6/15.
+//  Copyright (c) 2015 ArthurJiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
